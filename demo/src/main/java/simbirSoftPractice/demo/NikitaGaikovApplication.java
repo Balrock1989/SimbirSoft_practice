ec9bbb1bb73c1415cb19685ac9fee181ef9b22e7
Nikita <gaikovgnv@gmail.com>
2021-03-07 20:00:24 +0300

    edits made 2v

--- demo/src/main/java/simbirSoftPractice/demo/NikitaGaikovApplication.java
+++ demo/src/main/java/simbirSoftPractice/demo/NikitaGaikovApplication.java
@@ -18,13 +18,4 @@ public class NikitaGaikovApplication {
 		SpringApplication.run(NikitaGaikovApplication.class, args);
 	}
 
-//	@Bean
-//	public Docket api() {
-//		return new Docket(DocumentationType.SWAGGER_2)
-//				.select()
-//				.apis(RequestHandlerSelectors.any())
-//				.paths(PathSelectors.any())
-//				.build();
-//	}
-
 }
